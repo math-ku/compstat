8f0b26715c410200171b85e4cbee3616894b1ce4
Johan Larsson <johanlarsson@outlook.com>
2024-08-31 22:20:17 +0200

    feat: update intro slides

---
+++ tests/test_count_zeros.R
@@ -0,0 +1,5 @@
+test_that("count_zeros work on various input", {
+  expect_equal(count_zeros(c(0, 0, 1e-9, 25)), 2)
+  expect_equal(count_zeros(c(-0, 1.1, -2)), 1)
+  expect_equal(count_zeros(c()), 0)
+})
