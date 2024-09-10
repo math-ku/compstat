a1526f00e2d0d8e79e041615adeb390d7ac31023
Johan Larsson <johanlarsson@outlook.com>
2024-09-10 14:52:08 +0200

    chore: comitt wip profile script

--- R/profile.R
+++ R/profile.R
@@ -1,3 +1,15 @@
-x <- rnorm(100000)
+library(profvis)
 
-gauss_step(x, 0.2)
+x <- rnorm(1e3)
+
+kern_dens(x, 0.2)
+
+kern_dens_detail(x, 0.2)
+
+n <- 1
+p <- 100000
+
+x <- matrix(rnorm(n * p), n)
+y <- rnorm(n)
+
+matrix_vector_dot(x, y)
