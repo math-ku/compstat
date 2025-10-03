67bf8a95ff7cfc6be7f760b07601514f09a11102
Johan Larsson <jolars@posteo.com>
2025-10-03 16:41:05 +0200

    Update lecture 10

--- R/em_gauss_mix.R
+++ R/em_gauss_mix.R
@@ -1,61 +1,25 @@
-EM_gauss_mix <- function(x) {
-  n <- length(x)
-
-  EStep <- function(par) {
-    p <- par[1]
-    mu1 <- par[2]
-    mu2 <- par[3]
-    a <- p * exp(-(x - mu1)^2 / (2 * sigma1^2)) / sigma1
-    b <- (1 - p) * exp(-(x - mu2)^2 / (2 * sigma2^2)) / sigma2
-    a / (a + b)
-  }
-
-  MStep <- function(p_hat) {
-    N1 <- sum(p_hat)
-    N2 <- n - N1
-    c(N1 / n, sum(p_hat * x) / N1, sum((1 - p_hat) * x) / N2)
-  }
-
-  function(par, epsilon = 1e-12, maxit = 50, cb = NULL) {
-    for (i in 1:maxit) {
-      par0 <- par
-      par <- MStep(EStep(par))
-      if (!is.null(cb)) {
-        cb()
-      }
-      if (sum((par - par0)^2) <= epsilon * (sum(par^2) + epsilon)) break
-    }
-    par
-  }
+e_step_gauss <- function(par, x, sigma1 = 1, sigma2 = sigma1) {
+  p <- par[1]
+  mu1 <- par[2]
+  mu2 <- par[3]
+  a <- p * exp(-(x - mu1)^2 / (2 * sigma1^2)) / sigma1
+  b <- (1 - p) * exp(-(x - mu2)^2 / (2 * sigma2^2)) / sigma2
+  a / (a + b)
 }
 
-EM_gauss_mix <- function(x) {
+m_step_gauss <- function(p_hat, x) {
   n <- length(x)
-
-  EStep <- function(par) {
-    p <- par[1]
-    mu1 <- par[2]
-    mu2 <- par[3]
-    a <- p * exp(-(x - mu1)^2 / (2 * sigma1^2)) / sigma1
-    b <- (1 - p) * exp(-(x - mu2)^2 / (2 * sigma2^2)) / sigma2
-    a / (a + b)
-  }
-
-  MStep <- function(p_hat) {
-    N1 <- sum(p_hat)
-    N2 <- n - N1
-    c(N1 / n, sum(p_hat * x) / N1, sum((1 - p_hat) * x) / N2)
-  }
-
-  function(par, epsilon = 1e-12, maxit = 50, cb = NULL) {
-    for (i in 1:maxit) {
-      par0 <- par
-      par <- MStep(EStep(par))
-      if (!is.null(cb)) {
-        cb()
-      }
-      if (sum((par - par0)^2) <= epsilon * (sum(par^2) + epsilon)) break
-    }
-    par
-  }
+  N1 <- sum(p_hat)
+  N2 <- n - N1
+  c(N1 / n, sum(p_hat * x) / N1, sum((1 - p_hat) * x) / N2)
 }
+
+EM_gauss_mix_step <- function(x, sigma1 = 1, sigma2 = sigma1) {
+  force(x)
+  force(sigma1)
+  force(sigma2)
+
+  function(par) m_step_gauss(e_step_gauss(par, x, sigma1, sigma2), x)
+}
+
+em_gauss_step <- EM_gauss_mix_step(x, sigma1, sigma2)
